b4643f0e44fc3ddc30064f57abb99f62db2f7f15
Kittiphop Banklangnimit กิตติภพ บ้านกลางนิมิต (Saint) <gm456789123@gmail.com>
2026-06-21 19:30:46 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'data' );
+define( 'DB_NAME', 'demo' );
 
 /** Database username */
 define( 'DB_USER', 'mysql' );
