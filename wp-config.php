0bedaf5f6d32789b8d2a2e70eca274fa6a5decc9
Kittiphop Banklangnimit กิตติภพ บ้านกลางนิมิต (Saint) <gm456789123@gmail.com>
2026-06-23 20:42:39 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -25,13 +25,13 @@
 define( 'DB_NAME', getenv( 'WORDPRESS_DB_NAME' ) ?: 'data' );
 
 /** Database username */
-define( 'DB_USER', getenv( 'WORDPRESS_DB_USER' ) ?: 'root' );
+define( 'DB_USER', getenv( 'WORDPRESS_DB_USER' ) ?: 'mysql' );
 
 /** Database password */
 define( 'DB_PASSWORD', getenv( 'WORDPRESS_DB_PASSWORD' ) ?: '' );
 
 /** Database hostname */
-define( 'DB_HOST', getenv( 'WORDPRESS_DB_HOST' ) ?: 'localhost' );
+define( 'DB_HOST', getenv( 'WORDPRESS_DB_HOST' ) ?: 'mysql' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
